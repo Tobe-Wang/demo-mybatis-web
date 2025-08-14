e216d8843548e841fcaef54e1def3e25b6a1c654
Tobe-Wang <hzbeq@qq.com>
2025-08-14 14:23:09 +0800

    feat: 初始化示例项目
    - 添加 Maven 项目配置文件 pom.xml
    - 创建 Spring Boot 应用入口类 DemoMybatisWebApplication- 设置应用属性 spring.application.name=demo-mybatis-web
    - 添加 MySQL 和 Redis 依赖
    - 配置 Git 属性和忽略文件- 添加 Maven Wrapper 脚本和配置

---
+++ src/main/java/cn/zhaofd/demomybatisweb/DemoMybatisWebApplication.java
@@ -0,0 +1,11 @@
+package cn.zhaofd.demomybatisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMybatisWebApplication.class, args);
+    }
+}
