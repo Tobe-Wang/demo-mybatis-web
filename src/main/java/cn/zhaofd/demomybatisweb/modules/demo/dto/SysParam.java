8763514bf5bcd1bc28c5b64172f4a017ade7f50f
Tobe-Wang <hzbeq@qq.com>
2025-08-18 16:49:57 +0800

    feat(demo): 添加系统参数配置类和仓库接口
    - 新增 SysParam 类，用于系统参数配置
    - 添加 SysParamRepository 接口，用于系统参数的持久化操作

---
+++ src/main/java/cn/zhaofd/demomybatisweb/modules/demo/dto/SysParam.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demomybatisweb.modules.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 注解方式配置的MyBatis应用示例
+ */
+@Data
+public class SysParam implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+
+    @Size(max = 10)
+    private String code;
+
+    @Size(max = 10)
+    private String value;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Spring框架注解，用于前端到后端的数据绑定（HTTP请求参数->Java对象）
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Jackson库注解，用于JSON序列化和反序列化（Java对象<->JSON字符串）
+    private LocalDateTime regtime;
+}
---
+++ src/main/java/cn/zhaofd/demomybatisweb/modules/demo/repository/SysParamRepository.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demomybatisweb.modules.demo.repository;
+
+public interface SysParamRepository {
+}
